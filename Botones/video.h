8aa51bc5db28a75346624d3d91c47393a66e562d
DanielTorre <daniel.torre@alumnos.unican.es>
2020-02-06 16:44:56 +0100

    Test botones

---
+++ Botones/video.h
@@ -0,0 +1,27 @@
+#ifndef VIDEO_H
+
+#define VIDEO_H
+#include "regs.h"
+
+#define SCREEN_WIDTH   240
+#define SCREEN_HEIGHT  160
+
+#define vid_mem     ((u16*)MEM_VRAM)
+
+INLINE void m3_plot(int x, int y, COLOR clr)
+{   vid_mem[y*SCREEN_WIDTH+x]= clr;    }
+
+#define CLR_BLACK   0x0000
+#define CLR_RED     0x001F
+#define CLR_LIME    0x03E0
+#define CLR_YELLOW  0x03FF
+#define CLR_BLUE    0x7C00
+#define CLR_MAG     0x7C1F
+#define CLR_CYAN    0x7FE0
+#define CLR_WHITE   0x7FFF
+
+
+INLINE COLOR RGB15(u32 red, u32 green, u32 blue)
+{   return red | (green<<5) | (blue<<10);   }
+
+#endif
